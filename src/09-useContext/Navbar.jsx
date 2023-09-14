e5083f4ca9dead20e8e3000567293bc2c6514782
juan18506 <juan185067@gmail.com>
2023-09-13 22:00:44 -0300

    Added NavLinks

--- src/09-useContext/Navbar.jsx
+++ src/09-useContext/Navbar.jsx
@@ -1,11 +1,42 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
-    <>
-      <Link to="/react-hook-app/">Home</Link>
-      <Link to="/react-hook-app/about">About</Link>
-      <Link to="/react-hook-app/login">Login</Link>
-    </>
+  <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">Navbar</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink
+              className={({ isActive }) => `nav-link ${ isActive ? 'active': '' }`}
+              to="/react-hook-app/"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              className={({ isActive }) => `nav-link ${ isActive ? 'active': '' }`}
+              to="/react-hook-app/about"
+            >
+              About
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              className={({ isActive }) => `nav-link ${ isActive ? 'active': '' }`}
+              to="/react-hook-app/login"
+            >
+              Login
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   )
 }
