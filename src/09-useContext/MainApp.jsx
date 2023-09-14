bfcb89a45f8443f90953dc10d8066d008a9a9a34
juan18506 <juan185067@gmail.com>
2023-09-13 21:42:18 -0300

    Created routes

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,8 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import { HomePage } from "./HomePage";
+import { AboutPage } from "./AboutPage";
+import { LoginPage } from "./LoginPage";
+import { Navigate } from "react-router-dom";
+
 export const MainApp = () => {
   return (
     <>
       <h1>MainApp</h1>
       <hr />
+
+      <Routes>
+        <Route path="/react-hook-app/" element={ <HomePage /> } />
+        <Route path="/react-hook-app/about" element={ <AboutPage /> } />
+        <Route path="/react-hook-app/login" element={ <LoginPage /> } />
+
+        <Route path="/react-hook-app/*" element={ <Navigate to="/react-hook-app/about" /> } />
+      </Routes>
     </>
   )
 }
