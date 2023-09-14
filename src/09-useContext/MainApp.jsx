b7657b5133a3fc9ac932c239a7d285bec14d721d
juan18506 <juan185067@gmail.com>
2023-09-13 21:45:51 -0300

    Created Links

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -3,12 +3,17 @@ import { HomePage } from "./HomePage";
 import { AboutPage } from "./AboutPage";
 import { LoginPage } from "./LoginPage";
 import { Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const MainApp = () => {
   return (
     <>
       <h1>MainApp</h1>
+      <Link to="/react-hook-app/">Home</Link>
+      <Link to="/react-hook-app/about">About</Link>
+      <Link to="/react-hook-app/login">Login</Link>
       <hr />
+
 
       <Routes>
         <Route path="/react-hook-app/" element={ <HomePage /> } />
